4bbe1957b722129bcd6944bb91e2e1f002a81ba2
admkcdn <admkcdn@gmail.com>
2022-06-11 16:01:01 +0300

    updated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -24,4 +24,7 @@ foreach (var carColor in productManager.GetCarsByBrandId(1))
 
 
 CarManager carManager = new CarManager(new EfCarDal());
-carManager.Add(new Car {BrandId = 1, ColorId = 4,  DailyPrice = 130000, ModelYear = 1995, Description = "Sedan" });
+carManager.Add(new Car {BrandId = 3, ColorId = 1,  DailyPrice = 123456, ModelYear = 2018, Description = "Dizel" });
+carManager.Add(new Car {BrandId = 2, ColorId = 2,  DailyPrice = 163456, ModelYear = 2017, Description = "Elektrikli" });
+carManager.Add(new Car {BrandId = 4, ColorId = 3,  DailyPrice = 128456, ModelYear = 2019, Description = "formula 1 arabası diceklermiş son anda vazgeçmişler" });
+carManager.Add(new Car {BrandId = 3, ColorId = 2,  DailyPrice = 113456, ModelYear = 2022, Description = "Fenasal" });
